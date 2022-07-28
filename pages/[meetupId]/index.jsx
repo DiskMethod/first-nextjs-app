72982ba0164ab71700139ec9f8a7159ff550164e
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-07-28 19:31:11 -0400

    Fixed fallback

--- pages/[meetupId]/index.jsx
+++ pages/[meetupId]/index.jsx
@@ -27,7 +27,7 @@ export const getStaticPaths = async () => {
   const meetupsIds = await meetupsCollection.find({}, { _id: 1 }).toArray();
   client.close();
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetupsIds.map((meetupsId) => ({
       params: {
         meetupId: meetupsId._id.toString(),
@@ -54,7 +54,6 @@ export const getStaticProps = async (context) => {
         description: meetup.description,
       },
     },
-    revalidate: 1,
   };
 };
 
